7d02e61f7606693e5a4a25eeed4b11a9f954ba31
Eniola Ajiboye <eloyjaws@gmail.com>
2021-10-20 01:29:56 +0200

    Add streamlit app v1

--- app.py
+++ app.py
@@ -8,36 +8,175 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import altair as alt
 import streamlit as st
+from PIL import Image
 
 import utils
 import plots
+from utils import get_plot
 import text
 
-covid_data = utils.get_covid_data()
-dataset = utils.preprocess_covid_data(covid_data)
+# Get Dataset
+countries_data, continents_data = utils.getDataset()
 
-countries_data, continents_data = utils.country_continental_split(dataset)
+# Use Wide Page Forma
+# st.set_page_config(layout="wide")
 
-top_10_vaccinations_by_countries = utils.sort_and_return_top_k(countries_data)
-
-chart = plots.plot_bars(
-    top_10_vaccinations_by_countries,
-    "Total Vaccinations:Q",
-    "Location:O",
-    plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
-)
-
+logo = Image.open("logo.png")
+st.image(logo, width=100)
 
 st.title(text.Title)
 
 st.write(text.Paragraph_1)
-st.write("Charts are missing a Title")
-
-st.write(chart)
+st.info(text.Paragraph_2)
 
 st.header("Questions")
+st.subheader(
+    "We performed some EDA to gather some insights that can answer some of the questions listed below"
+)
 st.markdown(text.Questions)
+st.success(
+    "The Visualizations are organized in a way that present our findings and answers to the questions"
+)
+
+
+k_continents = 8
+st.sidebar.subheader("Country Parameters")
+COUNTRIES_SELECTED = st.sidebar.multiselect(
+    "Filter/Select Countries", utils.extract_location_list(countries_data)
+)
+k_countries = st.sidebar.slider("Number of Top countries", 5, 20, 10)
+
+st.sidebar.subheader("Render Menu")
+show_total_vaccinations = st.sidebar.checkbox("Number of Vaccinations", True)
+show_vaccinations_rates = st.sidebar.checkbox("Vaccination Rates", True)
+show_fully_vaccinated = st.sidebar.checkbox("Full Vaccinations")
+show_one_vaccination = st.sidebar.checkbox("One Vaccination")
+
+
+if show_total_vaccinations:
+    st.subheader(f"Vaccinations per continent")
+    st.altair_chart(
+        get_plot(
+            continents_data,
+            "Total Vaccinations:Q",
+            "Location:O",
+            k_continents,
+            sort_by=["Total Vaccinations"],
+            plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
+        ),
+        use_container_width=True,
+    )
+
+    st.subheader(f"Top {k_countries} Countries where Vaccinations were administered")
+    st.altair_chart(
+        get_plot(
+            countries_data,
+            "Total Vaccinations:Q",
+            "Location:O",
+            k_countries,
+            sort_by=["Total Vaccinations"],
+            plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
+        ),
+        use_container_width=True,
+    )
+
+    st.subheader(f"Bottom {k_countries} Countries where Vaccinations were administered")
+    st.altair_chart(
+        get_plot(
+            countries_data,
+            "Total Vaccinations:Q",
+            "Location:O",
+            k_countries,
+            sort_by=["Total Vaccinations"],
+            plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
+            ascending=True
+        ),
+        use_container_width=True,
+    )
+
+if show_vaccinations_rates:
+    st.subheader("Full Vaccination Rates per Continent")
+    st.altair_chart(
+        get_plot(
+            continents_data,
+            "Percentage Fully Vaccinated:Q",
+            "Location:O",
+            k_continents,
+            sort_by=["Percentage Fully Vaccinated"],
+            plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
+        ),
+        use_container_width=True,
+    )
+
+    continent_of_choice = st.selectbox("Filter/Select Continent", utils.extract_location_list(continents_data))
+    new_countries = utils.get_countries_by_continent(countries_data, continent_of_choice)
+    st.subheader(f"Top {k_countries} Fully Vaccinated countries in {continent_of_choice}")
+    st.altair_chart(
+        get_plot(
+            new_countries,
+            "Percentage Fully Vaccinated:Q",
+            "Location:O",
+            k_countries,
+            sort_by=["Percentage Fully Vaccinated"],
+            plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
+        ),
+        use_container_width=True,
+    )
+
+    st.subheader(f"Full Vaccination Rates per Country - Top {k_countries}")
+    st.altair_chart(
+        get_plot(
+            countries_data,
+            "Percentage Fully Vaccinated:Q",
+            "Location:O",
+            k_countries,
+            sort_by=["Percentage Fully Vaccinated"],
+            plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
+        ),
+        use_container_width=True,
+    )
+
+    st.subheader(f"Full Vaccination Rates per Country - Bottom {k_countries}")
+    st.altair_chart(
+        get_plot(
+            countries_data,
+            "Percentage Fully Vaccinated:Q",
+            "Location:O",
+            k_countries,
+            sort_by=["Percentage Fully Vaccinated"],
+            plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
+            ascending=True
+        ),
+        use_container_width=True,
+    )
+
+if show_fully_vaccinated:
+    st.subheader(f"Top {k_countries} Countries based on number of Full Vaccinations")
+    st.altair_chart(
+        get_plot(
+            countries_data,
+            "Total Vaccinations:Q",
+            "Location:O",
+            k_countries,
+            sort_by=["Total Vaccinations"],
+            plot_type=plots.PLOT_TYPES.FULLY_VACCINATED,
+        ),
+        use_container_width=True,
+    )
 
 
 st.header("Conclusion")
 st.write(text.Conclusion)
+
+
+with st.expander("References"):
+    st.write("References")
+
+st.sidebar.markdown(text.Footer)
+
+# col1, col2 = st.columns([3, 1])
+
+# with col1:
+#     st.write("Col 1")
+
+# col2.write("Col 2")
--- text.py
+++ text.py
@@ -1,15 +1,23 @@
-Title = "COVID-19 Vaccination Statistics"
+Title = "COVID-19 WorldWide Vaccination Statistics"
+Footer = """
+    An Interactive Web App by 
+    - Eniola Ajiboye (eajiboye) 
+    - Rukayat Sadiq (rsadiq)
+"""
+SidebarTitle = "Inputs"
 
-Paragraph_1 = ""
-Paragraph_2 = ""
+Paragraph_1 = "This app provides basic key metrics which can be used to assess the vacination rate/status of countries worldwide."
+Paragraph_2 = "Covid Vacination Data was obtained from [Our World In Data](https://ourworldindata.org/covid-vaccinations)"
 Paragraph_3 = ""
 Paragraph_4 = ""
 Paragraph_5 = ""
 
 Questions = """
-                1. Q1
-                2. Q2
-                3. Q3
+                1. What are the Top Countries where Vaccinations were Administered?
+                2. What are the Vaccination Rates Across these Countries?
+                3. What are the Vaccination Rates Across the Continents?
+                4. Which Countries have the highest number of administered first doses?
+                5. Does a Country's GDP play a role in the Vaccination rate across it's population?
             """
 
 Conclusion = """
